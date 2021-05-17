7c393735946cb065a4bee22e0d13872a64665d13
osvaldo william <osvaldowilliam@outlook.com>
2021-05-17 09:06:30 -0300

    Teste

--- application.py
+++ application.py
@@ -3,9 +3,11 @@ import os
 import pymongo as pymongo
 from fastapi import FastAPI
 from fastapi.openapi.models import Response
+from starlette import status
 
 import model
 from apis import ChairInfoApi, UserApi
+from utils import generateHash
 
 application = FastAPI()
 
@@ -28,4 +30,15 @@ async def log_info(chair_info: model.ChairInfo):
 
 @application.post('/create/user')
 async def create_user(user_info: model.UserInfo, response: Response):
-    return user_apis.create_user(user_info=user_info, response=response)
+    if hasattr(user_info, 'id'):
+        delattr(user_info, 'id')
+    users_db = ClIENT.users
+    is_present = users_db['users'].find_one({"email": user_info.email})
+    if is_present is not None:
+        response.status_code = status.HTTP_200_OK
+        return {"status": 'fail'}
+    user_info.password = generateHash(user_info.password).encode('utf-8')
+    new = users_db['users'].insert_one(user_info.dict(by_alias=True))
+    user_info.id = new.inserted_id
+    response.status_code = status.HTTP_201_CREATED
+    return {'status': 'success', 'data': user_info}
