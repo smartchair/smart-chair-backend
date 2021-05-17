ef8f9c05aa9758404c235ba2fbe59dc27bbc9547
osvaldo william <osvaldowilliam@outlook.com>
2021-05-17 10:50:52 -0300

    hash verification

--- utils/passUtils.py
+++ utils/passUtils.py
@@ -5,7 +5,7 @@ from typing import Any
 
 
 def generateHash(word: Any):
-    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    salt = hashlib.sha256(os.urandom(64)).hexdigest().encode('ascii')
     pwdhash = hashlib.pbkdf2_hmac('sha512', word.encode('utf-8'),
                                   salt, 100000)
     pwdhash = binascii.hexlify(pwdhash)
