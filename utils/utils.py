36759c4553970c2e92403d17da5a0c0f8c31c38e
osvaldo william <osvaldowilliam@outlook.com>
2021-08-16 11:01:40 -0300

    debug

--- utils/utils.py
+++ utils/utils.py
@@ -29,37 +29,37 @@ def getMeans(dates: [], means: []):
     returns = []
     for value in means:
         doc = value["doc"]
-        print(doc)
         day = datetime.strftime(value['dateTime'], "%d-%m-%y")
         if day == dates[0]:
             value0[0].append(doc['temp'])
             value0[1].append(doc['lum'])
             value0[2].append(doc['hum'])
             value0[3].append(doc['noise'])
-            returns.append(returnAverageProps(value0, day))
         if day == dates[1]:
             value1[0].append(doc['temp'])
             value1[1].append(doc['lum'])
             value1[2].append(doc['hum'])
             value1[3].append(doc['noise'])
-            returns.append(returnAverageProps(value1, day))
         if day == dates[2]:
             value2[0].append(doc['temp'])
             value2[1].append(doc['lum'])
             value2[2].append(doc['hum'])
             value2[3].append(doc['noise'])
-            returns.append(returnAverageProps(value2, day))
         if day == dates[3]:
             value3[0].append(doc['temp'])
             value3[1].append(doc['lum'])
             value3[2].append(doc['hum'])
             value3[3].append(doc['noise'])
-            returns.append(returnAverageProps(value3, day))
         if day == dates[4]:
             value4[0].append(doc['temp'])
             value4[1].append(doc['lum'])
             value4[2].append(doc['hum'])
             value4[3].append(doc['noise'])
-            returns.append(returnAverageProps(value4, day))
+
+    returns.append(returnAverageProps(value0, dates[0]))
+    returns.append(returnAverageProps(value1, dates[1]))
+    returns.append(returnAverageProps(value2, dates[2]))
+    returns.append(returnAverageProps(value3, dates[3]))
+    returns.append(returnAverageProps(value4, dates[4]))
 
     return returns
