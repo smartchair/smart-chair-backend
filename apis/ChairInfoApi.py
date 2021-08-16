7d9a139888acf26cf7a50f875b0dee2a7ce8e61c
osvaldo william <osvaldowilliam@outlook.com>
2021-08-16 08:30:05 -0300

    debug

--- apis/ChairInfoApi.py
+++ apis/ChairInfoApi.py
@@ -101,6 +101,7 @@ class ChairInfoApi:
         else:
             for doc in chairs:
                 day_doc = datetime.strptime(doc['time'], '%d-%m-%y %H:%M:%S')
+                print(day_doc)
                 if day_doc in dates:
                     item = {"dateTime": day_doc,
                             prop: doc[prop]}
